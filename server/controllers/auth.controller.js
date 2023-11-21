6de9d7a0313e537cd7e570482d83ea2208121af6
Neka <nekoanchor@yahoo.fr>
2023-11-21 12:44:54 +0100

    feat: add login logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,57 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+import { Users } from '../models/users.model.js';
+
+
 const login = async (req, res) => {
-    // create session
+    const { email, password, confirmPassword } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Missing email and/or password",
+        });
+    }
+
+    if(password !== confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Password and confirm password do not match",
+        });
+    }
+
+
+    const user = Users.find({ email }).first();
+
+    if (!user) {
+        return res.status(400).json({
+            success: false,
+            message: "Incorrect email or password",
+        });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Incorrect email or password",
+        });
+    }
+
+    const token = jwt.sign({
+        id : user._id,
+        email: user.email,
+    }, process.env.JWT_SECRET);
+
     res.json({
-        message: "Login",
+        success: true,
+        token,
     });
 }
 
 const logout = async (req, res) => {
-    // close session
     res.json({
         message: "Logout",
     });
--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -6,24 +6,92 @@ const { SALT_ROUNDS, APP_HOSTNAME } = process.env;
 import { signUpValidators, loginValidators } from "../utils/validators.js";
 
 const allUsers = (req, res) => {
-  // with and without filter
-  res.json({
-    message: "Users",
-  });
+  // exemple: http://localhost:4242/users?category=Mark&city=Toul
+  const params = req.query;
+  const filter = {};
+  if (params.name) {
+    filter.$or = [
+      { firstname: { $regex: params.name, $options: "i" } },
+      { lastname: { $regex: params.name, $options: "i" } },
+    ];
+  }
+  if (params.city) {
+    filter.city = { $regex: params.city, $options: "i" };
+  }
+  if (params.category) {
+    filter.category = { $regex: params.category, $options: "i" };
+  }
+
+  Users.find(filter)
+    .then((users) => {
+      res.json({
+        success: true,
+        data: users,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    });
 };
 
 const update = async (req, res) => {
-  // owner || admin
-  res.json({
-    message: "Update user",
-  });
+  const id = req.params.id;
+  const data = req.body;
+
+  const user = await Users.findById(id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  if (data.password) {
+    const password = await bcrypt.hash(data.password, parseInt(SALT_ROUNDS));
+    data.password = password;
+  }
+
+  const updatedUser = await Users.update({ _id: id }, { $set: data })
+    .then((user) => {
+      res.json({
+        success: true,
+        data: user,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    });
 };
 
-const remove = async (req, res) => {
-  // admin
-  res.json({
-    message: "Remove user",
-  });
+const remove = async (_, res) => {
+  const id = req.params.id;
+  const user = await Users.findById(id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User not found",
+    });
+  }
+
+  Users.remove({ _id: id })
+    .then((user) => {
+      res.json({
+        success: true,
+        data: user,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        error: error.message,
+      });
+    });
 };
 
 export { allUsers, update, remove };
